063b3561c6a18c6d93bc2a9d71468df299d5a24a
Naved Rasul <navedrasul@gmail.com>
2020-04-24 14:10:59 +0500

    Excel js integration (#19)
    * Merge pull request #15 from navedrasul/ExcelJS-Integration (#16)
    
    proceed!
    
    * transfered! (#17) (#18)
    
    ok
    
    * invoice-export component added

---
+++ src/app/invoice-export/invoice-export.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-export',
+  templateUrl: './invoice-export.component.html',
+  styleUrls: ['./invoice-export.component.scss']
+})
+export class InvoiceExportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
