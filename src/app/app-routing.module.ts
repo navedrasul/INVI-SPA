32cf031c27839322d816b9f01057e5b0a87b20d7
Naved Rasul <navedrasul@gmail.com>
2020-04-24 13:45:21 +0500

    transfered! (#17)
    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BodyComponent } from './components/layout/body/body.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {
-    path: '',
-    component: BodyComponent
+    path: '**',
+    component: HomeComponent
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { DataStorageService } from './services/data-storage.service';
-import { Item } from './models/item';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +8,4 @@ import { Item } from './models/item';
 export class AppComponent {
   title = 'INVI';
 
-  constructor(private dataSvc: DataStorageService) {
-    // Start: Testing code
-    // const items = [];
-
-    // for (let i = 0; i < 5; i++) {
-    //   items[i] = new Item({
-    //     name: 'This item name is intentionally too long to test the multi-row item name that could extend around three lines of text',
-    //     quantity: i + 1,
-    //     unit: '',
-    //     price: 13.35 * (2 * i + 1)
-    //   });
-    // }
-
-    // dataSvc.replaceAllItems(items);
-    // End: Testing code
-  }
 }
